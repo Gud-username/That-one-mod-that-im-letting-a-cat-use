1491e8d7a4d42a04f7c8e42eb03359889cf89489
Lusty joker <toby.hayter10@gmail.com>
2025-09-01 10:22:43 +0100

    Update credits.lua

--- credits.lua
+++ credits.lua
@@ -1,4 +1,4 @@
-SMODS.current_mod.extra_tabs = function()
+RSMODS.current_mod.extra_tabs = function()
     local credits_tab = {
       n = G.UIT.ROOT,
       config = { align = 'tl', padding = 0.05, emboss = 0.05, r = 0.1, colour = G.C.BLACK },
@@ -12,7 +12,7 @@ SMODS.current_mod.extra_tabs = function()
                   {
                     n = G.UIT.R,
                     nodes = {
-                      { n = G.UIT.T, config = { text = "Developer", colour = G.C.PLASMA, scale = 0.75 } },
+                      { n = G.UIT.T, config = { text = "Developer", colour = G.C.PERISHABLE, scale = 0.75 } },
                     }
                   },
                   {
@@ -31,14 +31,14 @@ SMODS.current_mod.extra_tabs = function()
               {
                 n = G.UIT.R,
                 nodes = {
-                  { n = G.UIT.T, config = { text = "Special thanks", colour = G.C.SECONDARY_SET[3], scale = 0.75 } },
+                  { n = G.UIT.T, config = { text = "Special thanks", colour = G.C.GOLD, scale = 0.75 } },
                 }
               },
               {
                 n = G.UIT.R,
                 config = { align = 'cm', minh = 0.25 },
                 nodes = {
-                  { n = G.UIT.T, config = { text = "Lily", colour = G.C.RARITY[3], scale = 0.4 } }
+                  { n = G.UIT.T, config = { text = "Lily", colour = G.C.MULT, scale = 0.4 } }
                 }
               },
             }
