c34b588675a1b4239629daee042a4f651c5537ef
Lusty joker <toby.hayter10@gmail.com>
2025-09-01 10:16:46 +0100

    Update credits.lua

--- credits.lua
+++ credits.lua
@@ -12,18 +12,38 @@ SMODS.current_mod.extra_tabs = function()
                   {
                     n = G.UIT.R,
                     nodes = {
-                      { n = G.UIT.T, config = { text = "Mod maker - Gud username", colour = G.C.CHIPS, scale = 0.75 } },
+                      { n = G.UIT.T, config = { text = "Developer", colour = G.C.PLASMA, scale = 0.75 } },
                     }
                   },
                   {
                     n = G.UIT.R,
                     config = { align = 'cm', minh = 0.25 },
                     nodes = {
-                      { n = G.UIT.T, config = { text = 'Specila thanks to Lily', colour = G.C.PURPLE, scale = 0.4 } }
+                      { n = G.UIT.T, config = { text = "Gud", colour = G.C.PURPLE, scale = 0.4 } }
                     }
                   },
                 }
               },
+          {
+            n = G.UIT.C,
+            config = { padding = 0.5 },
+            nodes = {
+              {
+                n = G.UIT.R,
+                nodes = {
+                  { n = G.UIT.T, config = { text = "Special thanks", colour = G.C.SECONDARY_SET[3], scale = 0.75 } },
+                }
+              },
+              {
+                n = G.UIT.R,
+                config = { align = 'cm', minh = 0.25 },
+                nodes = {
+                  { n = G.UIT.T, config = { text = "Lily", colour = G.C.RARITY[3], scale = 0.4 } }
+                }
+              },
+            }
+          },
+          
         }
       } }
     }
